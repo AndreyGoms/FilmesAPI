ae901db4d827acc7269e21650ed23d6f5b091998
Andrey Gomes <andreygm74@gmail.com>
2022-05-31 00:00:10 -0300

    Add project files.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,42 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]             // define a classe como controller
+    [Route("[Controller]")]     // define a rota pela qual ela será chamada, no caso pelo nome do controller
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 0;
+
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);            
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperarFilmes()
+        {
+            return filmes;
+        }
+
+        [HttpGet("{id}")]
+        public Filme RecuperaFilmePorId(int id)
+        {
+            foreach(Filme filme in filmes)
+            {
+                if (filme.Id == id)
+                    return filme;
+            }
+            return null;
+        }
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo titulo é obrigatorio")] // obriga a preencher o campo
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo diretor é obrigatorio")]
+        public string Diretor { get; set; }        
+
+        [StringLength(30, ErrorMessage = "genero = 30 char.")]
+        public string Genero { get; set; }
+        
+        [Range(1,600, ErrorMessage = "Fora do intervalo")]
+        public int Duracao { get; set; }
+    }
+}
+
+// "ErrorMessage=" : mensagem personalizada
+// "Required" : obriga o campo a ser preenchido 
